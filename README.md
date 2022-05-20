a7fd0304e6ab62708c753c685bfa2988f5b00c75
heysungj <linjing0525@gmail.com>
2022-05-20 16:44:06 -0400

    add more comments

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ As a user I want to
 .Be able to start a new game by clicking new game button
 .Be able to choose if I want an extra card or not
 .Be able to compare my score with dealer's score
+.Ace can be consider score 1 and score 11 based on your own insterest
 .If I have a score greater than 21 points I lose immediately
 .If I hit 21 points at the begining (with an ace and any card from 10 to king) I win immediately (Blackjack)
 .If I hava score between 1-21 and greater than dealer's score I win, otherwise I lose
--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 let playerCard = [];
 // dealerCard array to store dealer's points
 let dealerCard = [];
-let cardArr = [" ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
+// player
+currentPlayer = "you";
+// let cardArr = [" ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
 // generate random number from 1 -13
 let randomCard = () => {
   return Math.ceil(Math.random() * 13);
@@ -30,17 +32,22 @@ function startGame() {
   let dealerCard2 = document.createElement("img");
 
   // get player's first 2 cards and append them to playerCard container
+  // push score inside playerCard
   let random1 = randomCard();
   let random2 = randomCard();
   playerCard1.src = `./css/cards/${random1}.png`;
   playerCard2.src = `./css/cards/${random2}.png`;
   playerCard1.alt = `${random1}`;
   playerCard2.alt = `${random1}`;
-  console.log(playerCard1.alt);
+  // console.log(playerCard1.alt);
   playerCardContainer.appendChild(playerCard1);
   playerCardContainer.appendChild(playerCard2);
 
+  playerScore(random1);
+  playerScore(random2);
+  // console.log(playerCard);
   // get dealer's first 2 cards and append them to dealerCard container
+  // push score to dealerArr
   let random3 = randomCard();
   let random4 = randomCard();
   dealerCard1.src = `./css/cards/${random3}.png`;
@@ -49,7 +56,47 @@ function startGame() {
   dealerCard2.src = `./css/cards/${random4}.png`;
   dealerCardContainer.appendChild(dealerCard2);
   dealerCard2.alt = `${random4}`;
+  dealerScore(random3);
+  dealerScore(random4);
+  // console.log(dealerCard);
 }
+
+// game logic for cards from 2-10 it has its face value, for cards jack, queen, king they value 10
+// for card Ace it values either 1 and 11 based on your best interese
+function playerScore(random) {
+  if (random > 10) {
+    playerCard.push(10);
+  } else {
+    playerCard.push(random);
+  }
+}
+function dealerScore(random) {
+  if (random > 10) {
+    dealerCard.push(10);
+  } else {
+    dealerCard.push(random);
+  }
+}
+
+// when click hit button , player will be assigned another card
+
+hit.addEventListener("click", (currentPlayer) => {
+  if (currentPlayer === "you") {
+    let newCard = randomCard();
+    console;
+    let playerCard3 = document.createElement("img");
+    playerCard3.src = `./css/cards/${newCard}.png`;
+    playerCard3.alt = `${newCard}`;
+    playerCardContainer.appendChild(playerCard3);
+
+    playerScore(newCard);
+    console.log(playerCard);
+  }
+  // } else if (currentPlayer === 'dealer') {
+  //   let newCard = randomCard();
+  //   dealerScore(newCard)
+  // }
+});
 
 // when click start button, triger startGame both dealer and player will have 2 cards
 startBtn.addEventListener("click", () => {
